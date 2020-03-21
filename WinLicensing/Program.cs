2ce47fc1debf57c2604c87b678d99e1405468b93
zmjack <jack@zmland.com>
2020-03-21 22:56:11 +0800

    202003212256

--- WinLicensing/Program.cs
+++ WinLicensing/Program.cs
@@ -11,14 +11,16 @@ namespace WinLicensing
         static void Main(string[] args)
         {
             var bytes = Registry.GetValue(@"HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\ProductOptions", "ProductPolicy", null) as byte[];
-
             var policy = new ProductPolicy(bytes);
-            var values = policy.Values.OrderBy(x => x.Key);
+            var values = policy.Values.Where(x => x.Key.Contains("InBoxGames"));
+
             Echo.BorderTable(new[] { "Key", "Value" }, values.Select(v => new[]
             {
                 v.Key,
                 v.Value.Select(x => x.ToString("x2")+ " ").Join(""),
-            }).ToArray(), new[] { 52, 24 });
+            }).ToArray(), new[] { 48, 24 });
+
+            Console.ReadLine();
         }
     }
 }
